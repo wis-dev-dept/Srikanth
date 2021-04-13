89d82da7e45bfa2b7f3a24ddcdb415e6d73c1fef
wis-dev-dept <81128912+wis-dev-dept@users.noreply.github.com>
2021-04-13 22:39:58 +1200

    Update dbWIS.sql

--- dbWIS.sql
+++ dbWIS.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE dbbarber;
 --
+USE dbbarber;
 --
-CREATE TABLE 'tbcustomer' (
-    'FirstName' varchar(30) NOT NULL,
-    'LastName' varchar(30) NOT NULL,
-    'Phone' varchar(30) NOT NULL,
-    'Email' varchar(50) NOT NULL,
-    'Password' varchar(50) DEFAULT NULL
+
+CREATE TABLE `tbcustomer` (
+  `FirstName` varchar(30) NOT NULL,
+  `LastName` varchar(30) NOT NULL,
+  `Phone` varchar(50) DEFAULT NULL,
+  `Email` varchar(50) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
